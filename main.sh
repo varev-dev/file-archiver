af2e90692ef10eab8dbe02e3444403123fc3a7ff
varev-dev <doga.kacper@gmail.com>
2024-05-16 21:18:06 +0200

    infinite loop until user exit from main menu

--- main.sh
+++ main.sh
@@ -3,29 +3,37 @@
 
 TITLE="File Archiver"
 NO_FILES="No files were selected"
+FILE_SEPARATOR=","
 declare -a OPTIONS=("Archive" "Extract")
-
-selection=$(zenity --text "Choose option" --list --column=Menu "${OPTIONS[@]}" --title="$TITLE")
+declare -a selected_files=()
 
 function select_items() {
     local files
-    files=$(zenity --file-selection --multiple --separator="," --title="$TITLE")
+    files=$(zenity --file-selection --multiple --separator="$FILE_SEPARATOR" --title="$TITLE")
 
     if [ $? == 0 ]; then
-        IFS=":" read -r -a fileArray <<< "$files"
+        IFS="$FILE_SEPARATOR" read -r -a fileArray <<< "$files"
         echo "Selected files:"
         for file in "${fileArray[@]}"; do
             echo "$file"
+            selected_files+=("$file")
         done
     else
-       `zenity --info --text="$NO_FILES" --title="$TITLE"`
+        zenity --info --text="$NO_FILES" --title="$TITLE"
     fi  
 }
 
 while true; do
+    selection=$(zenity --text "Choose option" --list --column=Menu "${OPTIONS[@]}" --title="$TITLE");
+
+    if [ $? -ne 0 ]; then
+        break;
+    fi;
+
     if [ "$selection" == "Archive" ]; then
         echo "You selected Archive."
         select_items
+        echo "${selected_files[1]}"
     else
         echo "You selected Extract."
         # @TO-DO
